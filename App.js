ff6c18d209276ee364d1750e9f7bae2c884fc4e4
Harigovind2006 <73232525+Harigovind2006@users.noreply.github.com>
2023-05-17 08:13:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import WriteScreen from './screens/writeScreen'
+import ReadScreen from './screens/readScreen'
+
+
+var tabNavigator = createBottomTabNavigator({
+  WriteStory:{ screen:WriteScreen},
+  ReadStory:{ screen:ReadScreen}
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "ReadStory"){
+        return(
+          <Image
+          source={require("./assets/reading.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "WriteStory"){
+        return(
+          <Image
+          source={require("./assets/writing.jpg")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
+})
+
+export default createAppContainer(tabNavigator)
+
